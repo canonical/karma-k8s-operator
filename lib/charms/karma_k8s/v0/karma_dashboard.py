838938b03e56eb8847b575af1e15bcfbf870fdf8
Luca Bello <luca.bello@canonical.com>
2022-12-09 14:44:41 +0000

    bump version in karma library

--- lib/charms/karma_k8s/v0/karma_dashboard.py
+++ lib/charms/karma_k8s/v0/karma_dashboard.py
@@ -37,7 +37,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 3
+LIBPATCH = 4
 
 # Set to match metadata.yaml
 INTERFACE_NAME = "karma_dashboard"
