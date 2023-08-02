3dfcc7bc2a6310a6e158cc7ed2c9fcd8b86b8dd9
sed-i <82407168+sed-i@users.noreply.github.com>
2023-08-02 18:19:34 -0400

    Module-scope parametrization

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -76,43 +76,42 @@ def bundle_under_test(charm_under_test, tls_enabled: bool) -> str:
     return with_tls if tls_enabled else without_tls
 
 
-@pytest.mark.parametrize("tls_enabled", [False, True], scope="class")
-class TestCharm:
+@pytest.mark.parametrize("tls_enabled", [False, True], scope="module")
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy(ops_test, charm_under_test, tls_enabled):
+    """Deploy the charm-under-test and deploy it together with related charms."""
+    # GIVEN a karma bundle
+    await deploy_literal_bundle(ops_test, bundle_under_test(charm_under_test, tls_enabled))
 
-    @pytest.mark.abort_on_fail
-    async def test_build_and_deploy(self, ops_test, charm_under_test, tls_enabled):
-        """Deploy the charm-under-test and deploy it together with related charms."""
-        # GIVEN a karma bundle
-        await deploy_literal_bundle(ops_test, bundle_under_test(charm_under_test, tls_enabled))
+    # WHEN the deployment is settled
+    # THEN all apps are in active/idle
+    await ops_test.model.wait_for_idle(
+        status="active", raise_on_error=False, timeout=600, idle_period=30
+    )
 
-        # WHEN the deployment is settled
-        # THEN all apps are in active/idle
-        await ops_test.model.wait_for_idle(
-            status="active", raise_on_error=False, timeout=600, idle_period=30
-        )
+    # AND karma is able to communicate with alertmanager
+    # TODO when karma will have an HTTP API, query it to make sure alertmanager is reachable.
 
-        # AND karma is able to communicate with alertmanager
-        # TODO when karma will have an HTTP API, query it to make sure alertmanager is reachable.
 
-    @pytest.mark.abort_on_fail
-    async def test_remove(self, ops_test, tls_enabled):
-        # WHEN the apps are removed
-        apps = list(ops_test.model.applications.values())
-        logger.info("Removing apps: %s", apps)
-        for app in apps:
-            await app.destroy(destroy_storage=True, force=False, no_wait=False)
+@pytest.mark.abort_on_fail
+async def test_remove(ops_test):
+    # WHEN the apps are removed
+    apps = list(ops_test.model.applications.values())
+    logger.info("Removing apps: %s", apps)
+    for app in apps:
+        await app.destroy(destroy_storage=True, force=False, no_wait=False)
 
-        # THEN no app goes into error state and the model is empty
-        # TODO when the app removal Juju bug is fixed, replace the following with a wait_for_idle
-        #  with raise_on_error=True and assert len(ops_test.model.applications) == 0
+    # THEN no app goes into error state and the model is empty
+    # TODO when the app removal Juju bug is fixed, replace the following with a wait_for_idle
+    #  with raise_on_error=True and assert len(ops_test.model.applications) == 0
 
-        # Sometimes it take time to remove an app, and sometimes juju never really finishes
-        # removing. Sleep for a bit and then forcing removal.
-        await asyncio.sleep(30)
-        apps = list(ops_test.model.applications.values())
-        logger.info("Removing apps forcefully: %s", apps)
-        for app in apps:
-            await app.destroy(destroy_storage=True, force=True, no_wait=True)
-        await ops_test.model.block_until(lambda: len(ops_test.model.applications) == 0)
+    # Sometimes it take time to remove an app, and sometimes juju never really finishes
+    # removing. Sleep for a bit and then forcing removal.
+    await asyncio.sleep(30)
+    apps = list(ops_test.model.applications.values())
+    logger.info("Removing apps forcefully: %s", apps)
+    for app in apps:
+        await app.destroy(destroy_storage=True, force=True, no_wait=True)
+    await ops_test.model.block_until(lambda: len(ops_test.model.applications) == 0)
 
-        # Note: Removing all apps is also needed to clean the model for the next parametrization.
+    # Note: Removing all apps is also needed to clean the model for the next parametrization.
